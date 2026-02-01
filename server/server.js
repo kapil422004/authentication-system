3bd9f8d50bad6702700c144113734e3c8968f059
Kapil <kapilishwarkar2004@gmail.com>
2026-02-01 15:47:08 +0530

    fix minor bugs

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ await connect();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://authentication-system-virid.vercel.app/",
+  "https://authentication-system-virid.vercel.app",
 ];
 
 app.use(express.json());
